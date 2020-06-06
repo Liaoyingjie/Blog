9df7f7c1b9c516b3566b68e6825f852dce085390
lyj-w530 <lyjiwy@163.com>
2020-06-06 14:00:38 +0800

    定制admin界面

--- Article/admin.py
+++ Article/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-
-admin.site.register(Article)
+#定制admin界面
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id","title","content")
+    #注意逗号，正序排列，  -id 倒叙
+    ordering = ("id",)
+admin.site.register(Article,ArticleAdmin)
